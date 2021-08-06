f0277cd16a03cee662bda08e1951b35abc90e6e7
Matheus Lira <matheustlira@gmail.com>
2021-08-06 17:37:48 -0300

    post signIn working

--- src/controllers/userConroller.ts
+++ src/controllers/userConroller.ts
@@ -34,7 +34,7 @@ export async function authorizate(req: Request, res: Response) {
       return res.sendStatus(401);
     }
 
-    res.status(200).send(token);
+    res.status(200).send({ token });
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
